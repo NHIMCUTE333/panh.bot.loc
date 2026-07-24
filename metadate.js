5cb7f0734ea32027b27a8846a7207224d812cb1e
NHIMCUTE333 <haid00671@gmail.com>
2026-07-24 20:12:04 +0700

    Create metadate.js

---
+++ metadate.js
@@ -0,0 +1,6 @@
+{
+  "name": "Bánh Bột Lọc",
+  "description": "Bộ sưu tập và chia sẻ danh sách nhân vật, bot trò chuyện với bộ lọc thể loại, tìm kiếm và tương tác.",
+  "requestFramePermissions": [],
+  "majorCapabilities": ["MAJOR_CAPABILITY_SERVER_SIDE_GEMINI_API"]
+}
